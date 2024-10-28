358271a9f4135976c02c4af1900ef5ce617e0193
VINOTH <VINOTH@SAMNIVETHA-DELL>
2024-10-28 16:14:50 +0530

    Updated New Class Added

---
+++ com.demoshop/src/test/java/SeleniumPractice/GitHubClass_Test.java
@@ -0,0 +1,15 @@
+package SeleniumPractice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import com.GenericLibrary.Base_Class;
+
+public class GitHubClass_Test extends Base_Class {
+	
+	@Test
+	public void tesCase() {
+		
+		Reporter.log("GitHub TestCase Created",true);
+		
+	}
+}
